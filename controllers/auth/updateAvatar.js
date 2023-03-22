25af1d0c4ffe0089e91e42a4146a227122596c06
Serhii Lazarenok <serhiilazar90@gmail.com>
2023-03-22 22:32:10 +0200

    fix

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -7,6 +7,8 @@ const {User} = require("../../models/user");
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
+    
+    
     const {_id} = req.user;
     const {path: tempUpload, originalname} = req.file;
     const filename = `${_id}_${originalname}`;
@@ -18,7 +20,6 @@ const updateAvatar = async (req, res) => {
     await fs.rename(tempUpload,resultUpload);
     const avatarURL = path.join("avatars", filename);
     await User.findByIdAndUpdate(_id, {avatarURL});
-
     res.json({
         avatarURL,
     })
